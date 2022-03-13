b4ccb8b1d4f2784ba30831384f3df7636c3273b0
Dmaina5054 <dmain5054@gmail.com>
2022-03-13 11:38:50 +0300

    added heroku

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('portifolio/',include('resume.urls'))
+    path('',include('resume.urls'))
 ]
